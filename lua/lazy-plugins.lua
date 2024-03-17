49db5a4bc65bef3cc713e3e9433e96ba9bbb91fd
Joshua Ryan V. Velasquez <joshuaryan.velasquez.22@protonmail.com>
2024-03-17 21:58:34 +0800

    feat(plug): install & setup lualine

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -13,86 +13,86 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { import = "plugins" },
+  { import = "plugins" },
 
-     -- Core
-    'nvim-lua/plenary.nvim',
-     {
-         'numToStr/Comment.nvim',
-         config = function()
-             require("Comment").setup()
-         end,
-     },
-     'chrisbra/vim-diff-enhanced',
-     'junegunn/vim-easy-align',
-     'notjedi/nvim-rooter.lua',
-     --'nvim-treesitter/nvim-treesitter',
-     'RRethy/vim-illuminate',
-     'lukas-reineke/indent-blankline.nvim',
-     'nvim-tree/nvim-tree.lua',
-     'nvim-tree/nvim-web-devicons',
-     'tpope/vim-surround',
-     'tpope/vim-repeat',
-     'jiangmiao/auto-pairs',
+  -- Core
+  'nvim-lua/plenary.nvim',
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  'chrisbra/vim-diff-enhanced',
+  'junegunn/vim-easy-align',
+  'notjedi/nvim-rooter.lua',
+  --'nvim-treesitter/nvim-treesitter',
+  'RRethy/vim-illuminate',
+  'lukas-reineke/indent-blankline.nvim',
+  'nvim-tree/nvim-tree.lua',
+  'nvim-tree/nvim-web-devicons',
+  'tpope/vim-surround',
+  'tpope/vim-repeat',
+  'jiangmiao/auto-pairs',
 
-     -- Navigation
-     { 'mg979/vim-visual-multi', branch = 'master' },
-     'MattesGroeger/vim-bookmarks',
-     'psliwka/vim-smoothie',
-     'unblevable/quick-scope',
-     'nvim-tree/nvim-tree.lua',
+  -- Navigation
+  { 'mg979/vim-visual-multi', branch = 'master' },
+  'MattesGroeger/vim-bookmarks',
+  'psliwka/vim-smoothie',
+  'unblevable/quick-scope',
+  'nvim-tree/nvim-tree.lua',
 
-     -- Telescope
-     'nvim-lua/plenary.nvim',
-     'nvim-telescope/telescope.nvim',
+  -- Telescope
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope.nvim',
 
-     -- Copilot
-     'github/copilot.vim',
+  -- Copilot
+  'github/copilot.vim',
 
-     -- Linting
-     'mfussenegger/nvim-lint',
-     'digitaltoad/vim-pug',
-     'jwalton512/vim-blade',
-     'pangloss/vim-javascript',
-     'statico/vim-javascript-sql',
-     'vim-ruby/vim-ruby',
-     'godlygeek/tabular',
-     'MaxMEllon/vim-jsx-pretty',
-     'joereynolds/sql-lint',
-     'vim-python/python-syntax',
-     'tbastos/vim-lua',
-     'ap/vim-css-color',
-     { "folke/neodev.nvim", opts = {} },
+  -- Linting
+  'mfussenegger/nvim-lint',
+  'digitaltoad/vim-pug',
+  'jwalton512/vim-blade',
+  'pangloss/vim-javascript',
+  'statico/vim-javascript-sql',
+  'vim-ruby/vim-ruby',
+  'godlygeek/tabular',
+  'MaxMEllon/vim-jsx-pretty',
+  'joereynolds/sql-lint',
+  'vim-python/python-syntax',
+  'tbastos/vim-lua',
+  'ap/vim-css-color',
+  { "folke/neodev.nvim", opts = {} },
 
-     -- Git
-     'tpope/vim-fugitive',
-     --'f-person/git-blame.nvim',
-     'kdheepak/lazygit.nvim',
+  -- Git
+  'tpope/vim-fugitive',
+  --'f-person/git-blame.nvim',
+  'kdheepak/lazygit.nvim',
 
-     -- Theme
-     -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- Theme
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-     -- LSP
-     'williamboman/mason.nvim',
-     'williamboman/mason-lspconfig.nvim',
-     'neovim/nvim-lspconfig',
-     'onsails/lspkind.nvim',
+  -- LSP
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
+  'onsails/lspkind.nvim',
 
-     -- SNIPPET
-     'L3MON4D3/LuaSnip',
-     'hrsh7th/nvim-cmp',
-     'hrsh7th/cmp-nvim-lsp',
-     'hrsh7th/cmp-buffer',
-     'hrsh7th/cmp-path',
-     'hrsh7th/cmp-cmdline',
-     'rafamadriz/friendly-snippets',
-     'saadparwaiz1/cmp_luasnip',
+  -- SNIPPET
+  'L3MON4D3/LuaSnip',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'rafamadriz/friendly-snippets',
+  'saadparwaiz1/cmp_luasnip',
 
-     -- Etc
-     'tpope/vim-eunuch',
-     -- 'JoosepAlviste/nvim-ts-context-commentstring',
-     {
-         'stevearc/dressing.nvim',
-         opts = {}
-     }
+  -- Etc
+  'tpope/vim-eunuch',
+  -- 'JoosepAlviste/nvim-ts-context-commentstring',
+  {
+    'stevearc/dressing.nvim',
+    opts = {}
+  }
 })
