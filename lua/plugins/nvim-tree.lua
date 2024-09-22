5995cd479d3f828bcd97d504da33f5bf9684cb54
Joshua Ryan V. Velasquez <joshuaryan.velasquez.22@protonmail.com>
2024-09-22 18:23:17 +0800

    refactor(plug): update nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,12 +8,31 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup({
-        update_cwd = false,
-        update_focused_file = {
-          enable = true,
-          update_root = false
+      local tree = require("nvim-tree")
+
+      local custom_on_attach = function(bufnr)
+        local api = require "nvim-tree.api"
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- custom mappings
+        vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+        vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+      end
+
+      tree.setup({
+        git = {
+          enable = false,
+          ignore = false,
+          show_on_dirs = false,
+          timeout = 400,
         },
+        on_attach = custom_on_attach,
         renderer = {
           icons = {
             show = {
@@ -35,11 +54,10 @@ return {
             }
           }
         },
-        git = {
-          enable = false,
-          ignore = false,
-          show_on_dirs = false,
-          timeout = 400,
+        update_cwd = false,
+        update_focused_file = {
+          enable = true,
+          update_root = false
         },
         view = {
           width = 45
