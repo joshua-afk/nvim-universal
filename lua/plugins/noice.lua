99337593f98148ea85d888d8151fbe65abf75cc7
Joshua Ryan V. Velasquez <joshuaryan.velasquez.22@protonmail.com>
2024-10-23 04:12:18 +0800

    feat(plug): install & configure noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        presets = {
+          bottom_search = false,    -- use a classic bottom cmdline for search
+          command_palette = true,  -- position the cmdline and popupmenu together
+          long_message_to_split = false, -- long messages will be sent to a split
+          inc_rename = true,        -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,   -- add a border to hover docs and signature help
+        },
+        cmdline = {
+          enabled = true
+        },
+        messages = {
+          enabled = false
+        },
+      })
+    end,
+  },
+}
