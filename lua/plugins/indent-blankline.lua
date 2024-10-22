52be7de72d0a7cecf74f9f771fec9e7aa135f7a9
Joshua Ryan V. Velasquez <joshuaryan.velasquez.22@protonmail.com>
2024-10-23 04:11:32 +0800

    feat(style): thinner indent blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,7 +4,11 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-      require('ibl').setup()
+      require('ibl').setup({
+        indent = {
+          char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+        },
+      })
     end
   }
 }
