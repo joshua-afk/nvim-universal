1cedfe1d913f9c6273bbdfc12d6069cfb7afb1af
Joshua Ryan V. Velasquez <joshuaryan.velasquez.22@protonmail.com>
2024-10-23 05:06:29 +0800

    feat(plug): install & configure tmux

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,10 +1,25 @@
 return {
+  -- TODO: configure this more for seamless navigation between tmux and nvim
   "aserowy/tmux.nvim",
   config = function()
     return require("tmux").setup({
       copy_sync = {
-        enable = true
+        enable = true,
+        -- redirect_to_clipboard = true,
+        -- sync_registers_keymap_put = true,
+        -- sync_registers_keymap_reg = true,
+        -- sync_deletes = true,
+        -- sync_unnamed = true
       },
+      navigation = {
+        cycle_navigation = false,
+        enable_default_keybindings = false,
+      },
+      resize = {
+        -- enables default keybindings (A-hjkl) for normal mode
+        enable_default_keybindings = false,
+      }
     })
   end
 }
+
