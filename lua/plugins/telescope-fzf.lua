4b67c28e124b72bed72d18304edaa56b0b24ccc0
Joshua Ryan V. Velasquez <joshuaryan.velasquez.22@protonmail.com>
2024-10-23 04:56:59 +0800

    feat(plugin): update telescope configs

--- lua/plugins/telescope-fzf.lua
+++ lua/plugins/telescope-fzf.lua
@@ -1,6 +1,7 @@
 return {
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-  }
+  -- TODO: use only for windows and linux
+  -- {
+  --   'nvim-telescope/telescope-fzf-native.nvim',
+  --   build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  -- }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,17 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'kdheepak/lazygit.nvim',
-      'nvim-tree/nvim-web-devicons'
+      'nvim-tree/nvim-web-devicons',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make'
+      }
     },
+    priority = 1000,
+    tag = '0.1.8',
     config = function()
       local telescope = require('telescope')
 
@@ -38,6 +43,9 @@ return {
                 vim.api.nvim_input "<C-W>"
               end,
             },
+            n = {
+              ['<C-p>'] = require('telescope.actions.layout').toggle_preview
+            }
           }
         },
         extensions = {
@@ -51,6 +59,8 @@ return {
         },
         pickers = {
           find_files = {
+            -- find_command = { 'rg', '--files',  '-g', '--hidden', '!.git', '!.github' },
+            --lua require("telescope.builtin").find_files({ 'rg', '--files',  '-g', '--hidden', '!.git', '!.github' })
             previewer = false
             -- theme = "dropdown" -- results is messed up in large codebase
           },
@@ -60,9 +70,14 @@ return {
         },
       })
 
-      telescope.load_extension('lazygit')
+      -- Custom highlights
+      local colors = require("catppuccin.palettes").get_palette()
+      api.nvim_set_hl(0, "TelescopeBorder", { fg = colors.mauve })
+      api.nvim_set_hl(0, "TelescopeTitle", { fg = colors.lavender })
+
+      -- Extensions
       telescope.load_extension('fzf')
+      telescope.load_extension('lazygit')
     end
   }
-
 }
